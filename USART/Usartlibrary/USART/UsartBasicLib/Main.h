cbc18727791d524a33cd2ef650e45f5b7c0c4632
Alex <dante.l.levi93@gmail.com>
2017-10-19 16:18:46 +0400

    Базовая библиотека по работе с USART-интерфейсом

---
+++ USART/Usartlibrary/USART/UsartBasicLib/Main.h
@@ -0,0 +1,27 @@
+/*
+ * Main.h
+ *
+ * Created: 17.10.2017 11:35:03
+ *  Author: Dante_L_Levi
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+
+#include "USART.h"
+#include "CicleBuffer.h"
+
+
+
+
+#endif /* MAIN_H_ */
---
+++ USART/Usartlibrary/USART/UsartBasicLib/main.c
@@ -0,0 +1,16 @@
+
+
+#include "Main.h"
+
+
+
+int main(void)
+{
+    Usart_Init(9600);
+	Usart_str_rn("Hello World");
+
+    while (1) 
+    {
+    }
+}
+
